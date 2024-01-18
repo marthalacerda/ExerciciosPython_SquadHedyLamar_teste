f273c0beea20d76af4265cfaf539f6a9a0c6e65a
JhenyfferOliveira <jhenyffer.trad@gmail.com>
2024-01-18 20:09:46 -0300

    Adicionando exercicio 1

---
+++ Semana03-Desafio-Python/conceitos-iniciais/Exercicios.py
@@ -0,0 +1,19 @@
+﻿# Exercício 1
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+# Soma
+soma = n1 + n2
+print(f'A soma dos números é: {soma}')
+
+# Subtração
+subtracao = n1 - n2
+print(f'A subtração dos números é: {subtracao}')
+
+# Multiplicação
+multiplicacao = n1 * n2
+print(f'A multiplicação dos números é: {multiplicacao}')
+
+# Divisão
+divisao = n1 / n2
+print(f'A divisão dos números é: {divisao}')
