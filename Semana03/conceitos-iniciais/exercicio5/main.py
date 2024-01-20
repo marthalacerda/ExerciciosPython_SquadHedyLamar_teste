164cb63504f83b19a9beb3a2152d8c3e9693d5ad
Mariana Amaral <mariana_amaral@qat.com>
2024-01-20 08:21:07 -0500

    Solução exercicio 5 Removido exercicios já resolvidos

---
+++ Semana03/conceitos-iniciais/exercicio5/main.py
@@ -0,0 +1,34 @@
+# 5. Escreva um programa que calcule o salário líquido. Lembrando de
+# declarar o salário bruto e o percentual de desconto do Imposto de
+# Renda.
+# ● Renda até R$ 1.903,98: isento de imposto de renda;
+# ● Renda entre R$ 1.903,99 e R$ 2.826,65: alíquota de 7,5%;
+# ● Renda entre R$ 2.826,66 e R$ 3.751,05: alíquota de 15%;
+# ● Renda entre R$ 3.751,06 e R$ 4.664,68: alíquota de 22,5%;
+# ● Renda acima de R$ 4.664,68: alíquota máxima de 27,5%.
+
+def calculaSalarioLiquido(renda, percentual):
+  return float("{:.2f}".format(renda * percentual /100))
+
+renda = float(input('Informe a renda:\n'))
+
+if renda <= 1903.98:
+  print("Renda até R$ 1.903,98: isento de imposto de renda:\n", renda)
+
+if renda >= 1903.99 and renda <= 2826.65:
+  print("Renda entre R$ 1.903,99 e R$ 2.826,65: alíquota de 7,5%:\n R$", calculaSalarioLiquido(renda, 7.5))
+
+
+if renda >= 2826.66 and renda <= 3751.05:
+  print("Renda entre R$ 2.826,66 e R$ 3.751,05: alíquota de 15%:\n R$", calculaSalarioLiquido(renda, 15))
+
+
+if renda >= 3751.06 and renda <= 4664.68:
+  print("Renda entre R$ 3.751,06 e R$ 4.664,68: alíquota de 22,5%:\n R$", calculaSalarioLiquido(renda, 22.5))
+
+if renda > 4664.68:
+  print("Renda acima de R$ 4.664,68:\n R$", calculaSalarioLiquido(renda, 27.5))
+
+
+
+
--- Semana03/conceitos-iniciais/exercicio_2.py
+++
@@ -1,6 +0,0 @@
-# 2. Peça ao usuário para informar o ano de nascimento. Em seguida,
-# calcule e imprima a idade atual.
-
-anoNascimento = int(input('Entre com o seu ano de nascimento:\n'))
-
-print('Sua idade em 2024 é:', 2024 - anoNascimento, '\n')
