12b8bdd8ca641704d828ab71bdc1aed9246c77fd
Ferskys <fer2580sky@gmail.com>
2024-01-21 15:21:52 -0300

    Execício_06: comparativo tempo de viagem: task completa ✅

---
+++ Semana03/conceitos-iniciais/exercicio6/tempo_viagem.py
@@ -0,0 +1,28 @@
+# Distância total em km (você pode alterar este valor)
+
+distancia_total = 1000
+
+# Velocidades de viagem (km/h)
+
+velocidade_aviao = 600
+
+velocidade_carro = 100
+
+velocidade_onibus = 80
+
+
+# Cálculo do tempo de viagem para cada meio de transporte
+
+tempo_aviao = distancia_total / velocidade_aviao
+
+tempo_carro = distancia_total / velocidade_carro
+
+tempo_onibus = distancia_total / velocidade_onibus
+
+# Exibição dos resultados
+
+print("Tempo de viagem para avião: ", tempo_aviao, "h")
+
+print("Tempo de viagem para carro: ", tempo_carro, "h")
+
+print("Tempo de viagem para ônibus: ", tempo_onibus, "h")
