585587c66c1b1c4b5835e9f3b45aa21376924b49
Mariana Amaral <mariana_amaral@qat.com>
2024-01-23 18:23:24 -0500

    Solução exercicio 5

---
+++ Semana03/listas-tuplas-dicionarios/exercicio5/main.py
@@ -0,0 +1,10 @@
+# 5. Crie duas tuplas. Concatene-as para formar uma nova tupla.
+
+tupla1 = ("maça", "banana", "mamão")
+
+tupla2 = ("morango", "manga")
+
+tupla3 = tupla1 + tupla2
+
+print('Nova tupla: ',tupla3)
+
